122df724d51d72a1d6062c64d89f1319b2783c84
WeiLocus <marsuo52@gmail.com>
2023-06-08 11:35:49 +0800

    layout: add text and bg-style

--- globalStyles.js
+++ globalStyles.js
@@ -7,14 +7,17 @@ export const GlobalStyle = createGlobalStyle`
 
     /* font-size */
     --fs-basic: 1rem; 
-    --fs-secondary: 1.2rem;
+    --fs-big-100: 2rem;
+    --fs-big-200: 2.6rem;
+    --fs-big-500: 6rem;
     --fs-small: 0.8rem;
 
     /* colors */
     --color-bg: #ededed;
     --color-primary: #f9f9f9;
-    --color-secondary: #828282;
-    --color-temp: #4770d0;
+    --color-secondary: #3f3636;
+    --color-temp: #149ECA;
+
   }
 
   // css reset
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { GlobalStyle } from "../globalStyles"
-import { Container } from "./styles/style"
+import { Container, Card, TopCard, BottomCard, Location, Temperature, Celsius, Description, AirFlow, Rain, Refresh } from "./styles/style"
 
 function App() {
 
@@ -7,7 +7,26 @@ function App() {
     <>
     <GlobalStyle />
       <Container>
-
+        <Card>
+          <TopCard>
+            <Location>
+              台中市
+            </Location>
+            <Temperature>
+              30<Celsius>°C</Celsius>
+            </Temperature>
+          </TopCard>
+          <BottomCard>
+            <Description>
+              多雲時晴
+            </Description>
+            <AirFlow>
+              10m/h
+            </AirFlow>
+            <Rain>66%</Rain>
+            <Refresh>上午12:00</Refresh>
+          </BottomCard>
+        </Card>
       </Container>
     </>
   )
