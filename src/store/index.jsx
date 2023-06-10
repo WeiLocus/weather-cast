836c12441d6a73885c2ab9a743f73678064332dc
WeiLocus <marsuo52@gmail.com>
2023-06-10 23:06:00 +0800

    feat: add weatherApi reducer and middleware

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,8 +1,19 @@
 import { weatherReducer } from "./slice/weatherSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { weatherApi } from "./api/weatherApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
   reducer: {
-    weather: weatherReducer
+    weather: weatherReducer,
+    [weatherApi.reducerPath]: weatherApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware()
+      .concat(weatherApi.middleware)
   }
 })
+
+setupListeners(store.dispatch)
+
+export {useFetchWeatherQuery} from "./api/weatherApi"
