24474252298c7d7d4b7cb06b4a2c4c7bbbbc5865
WeiLocus <marsuo52@gmail.com>
2023-06-12 12:02:41 +0800

    import more icon

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components"
 import { ReactComponent as Rain } from "../assets/climate-cloud-rain.svg"
 import { ReactComponent as Strom } from "../assets/clouds-strom.svg"
-import { ReactComponent as CloudDay } from "../assets/cloudy-sun.svg"
+import { ReactComponent as DayCloud } from "../assets/cloudy-sun.svg"
 import { ReactComponent as ClimateRainDay } from "../assets/climate-forecast-rain-day.svg"
 import { ReactComponent as Day } from "../assets/sun.svg"
+import { ReactComponent as DayCloudSnow } from "../assets/sunny-cloud-snow.svg"
 
 import { ReactComponent as ClimateRainNight } from "../assets/climate-forecast-rain-night.svg"
-import { ReactComponent as CloudNight } from "../assets/cloud-night.svg"
+import { ReactComponent as NightCloud } from "../assets/cloud-night.svg"
 import { ReactComponent as Night } from "../assets/night.svg"
+import { ReactComponent as NoghtCloudSnow } from "../assets/cloud-night-snow.svg"
 
 const IconContainer = styled.div`
   position: absolute;
@@ -18,7 +20,7 @@ const IconContainer = styled.div`
 function WeatherIcon() {
   return (
     <IconContainer>
-      <CloudDay />
+      <DayCloud />
     </IconContainer>
   )
 }
